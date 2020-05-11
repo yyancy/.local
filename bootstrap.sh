f29d6899e91aef9d5d0c6dcc8ba465fb65d73130
yyancy <35031396+yyancy@users.noreply.github.com>
2020-05-11 16:37:13 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,24 @@
 # initialize new linux system.
 # currently it support ubuntu only.
 
+# replace apt source to aliyun source.
+APT_SOURCE=/etc/apt/sources.list
+APT_SOURCE_ALIYUN=
+
+cp $APT_SOURCE /etc/apt/sources.list.bak
+cp etc/sources.list.aliyun $APT_SOURCE
+
+sudo apt update
+
+which gcc
+if [ $? -ne 0 ] ; then
+    echo "not found gcc,install it."
+    apt install build-essential
+fi 
+
 which git
 if [ $? -ne 0 ] ; then
+  echo "not found git,install it."
   apt install git
 fi
 
