0ac01a2e58f763977a9d7294640dde6c7151c0ec
yyancy <yyancy517@gmail.com>
2020-05-14 21:05:17 +0800

    add intstall sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,14 @@
 # replace apt source to aliyun source.
 APT_SOURCE=/etc/apt/sources.list
 APT_SOURCE_ALIYUN=
+SOURCE=https://github.com/yyancy/.local.git
+ALICAS=.local
+USER_LOCAL=~/.local
+CURRENT_LOCAL=
+# cp $APT_SOURCE /etc/apt/sources.list.bak
+# cp etc/sources.list.aliyun $APT_SOURCE
 
-cp $APT_SOURCE /etc/apt/sources.list.bak
-cp etc/sources.list.aliyun $APT_SOURCE
-
-sudo apt update
+# sudo apt update
 
 which gcc
 if [ $? -ne 0 ] ; then
@@ -22,4 +25,30 @@ if [ $? -ne 0 ] ; then
   echo "not found git,install it."
   apt install git
 fi
+
+if [ -d ${USER_LOCAL} ] ; then
+  echo ".local has existed. change its name to .local.old ."
+  mv ${USER_LOCAL}  ~/.local.old
+fi
+
+# clone personal configuration to current directory.
+git clone ${SOURCE} ${ALICAS}
+CURRENT_LOCAL=`pwd`/${ALICAS}
+
+if [ "${CURRENT_LOCAL}" != "${USER_LOCAL}" ] ; then
+  cp -r ${CURRENT_LOCAL} ${USER_LOCAL} &> /dev/null
+fi
+
+# CP_FLAG=
+# for dir in `ls ${CURRENT_LOCAL}` ; do 
+#   [ -d ${dir} ] &&  CP_FLAG=-R 
+#   cp ${CURRENT_LOCAL}/${dir} ${USER_LOCAL}
+# done
+# unset CP_FLAG
+
+# invoke insall.sh
+chmod 755 ${USER_LOCAL}/install.sh
+${USER_LOCAL}/install.sh
+
+
 
